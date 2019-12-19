c5a9e15f2a8ca91514a972d9f303ce8cf3c22976
Lawrence Lagerlof <llagerlof@gmail.com>
2019-12-19 09:52:16 -0300

    Versao 1.0.1

--- MyClass.php
+++ MyClass.php
@@ -1,9 +1,9 @@
 <?php
-// Version 1.0.0
+// Version 1.0.1
 class MyClass
 {
     public function __construct()
     {
-        echo "Object of class MyClass, version 1.0.0, instantiated.\n";
+        echo "Object of class MyClass, version 1.0.1, instantiated.\n";
     }
 }
